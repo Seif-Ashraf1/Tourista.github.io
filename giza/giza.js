9fa15bb4708f602a05ae1ff704428abe40a117d6
Seif-Ashraf1 <164083282+Seif-Ashraf1@users.noreply.github.com>
2024-04-30 20:48:03 +0300

    sondos-resolution 1920x1080 scale 125%

---
+++ giza/giza.js
@@ -0,0 +1,38 @@
+const TrandingSlider = new Swiper('.tranding-slider', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const links = document.querySelectorAll('.navigation a');
+
+  links.forEach(function(link) {
+    link.addEventListener('click', function(event) {
+      event.preventDefault(); 
+
+      const targetId = this.getAttribute('href').substring(1);
+      const targetSection = document.getElementById(targetId);
+
+      if (targetSection) {
+        targetSection.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+});
